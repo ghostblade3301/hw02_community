01b0d66cafc5a65fce8798a3b427ab8447d66af0
Pavel Naumenko <ghostblade3301@gmail.com>
2022-11-12 22:43:00 +0300

    initial commit

---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    template = 'posts/index.html'
+    posts = (Post.objects.all()[:settings.POSTS_PER_PAGE])
+    context = {
+        'posts': posts
+    }
+    return render(request, template, context)
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.group_posts.all()[:settings.POSTS_PER_PAGE]
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
